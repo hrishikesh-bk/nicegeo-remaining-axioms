44f9eca7b46c5707e3f383bbfa4735c9e5d35080
pranavrajpal <78008260+pranavrajpal@users.noreply.github.com>
2026-02-09 11:10:38 -0600

    Implement inferType for functions and function applications (#4)
    * Implement inferring type of Lam
    
    * Add another Lam inferType test
    
    * Implement inferring type of function application
    
    * Write function for substituting value for Bvar
    
    * Handle applying generic functions
    
    Extend the handling of App to handle cases where the type of the argument to a function is itself a variable

--- lib/infer.ml
+++ lib/infer.ml
@@ -1,11 +1,30 @@
 open Term
 
-let inferType (env : environment) (localCtx : localcontext) (t : term) : term =
+(* Substitute the bound variable at index `bvar_idx` (relative to the top level
+term, so what would have been at index `bvar_idx` in the localcontext stack for
+the original term) with the provided value `bvar_val` in the term `t`*)
+let rec subst_bvar (t: term) (bvar_idx: int) (bvar_val: term) : term = 
+  match t with
+    | Const _ | Sort _ | Fvar _ -> t
+    | Bvar idx -> if bvar_idx = idx then bvar_val else Bvar idx
+    | Lam (domain_type, body) -> 
+        (* Add 1 to bvar_idx to account for the fact that a Lam introduces a bound
+        variable *)
+        Lam (subst_bvar domain_type bvar_idx bvar_val, subst_bvar body (bvar_idx + 1) bvar_val)
+    | Forall (domain_type, ret_type) ->
+        Forall (subst_bvar domain_type bvar_idx bvar_val, subst_bvar ret_type (bvar_idx + 1) bvar_val)
+    | App (func, arg) -> 
+        let func_subst = subst_bvar func bvar_idx bvar_val in
+        let arg_subst = subst_bvar arg bvar_idx bvar_val in
+        App (func_subst, arg_subst)
+
+let rec inferType (env : environment) (localCtx : localcontext) (t : term) : term =
   match t with
   | Const name -> (
       try Hashtbl.find env name
       with Not_found -> failwith ("unknown constant: " ^ name)
     )
+  (* TODO: remove this (since we're not using free variables anymore) *)
   | Fvar name -> (
       try Hashtbl.find env name
       with Not_found -> failwith ("unknown free variable: " ^ name)
@@ -15,7 +34,32 @@ let inferType (env : environment) (localCtx : localcontext) (t : term) : term =
       with Failure _ | Invalid_argument _ ->
         failwith ("bound variable index out of scope: " ^ string_of_int idx)
     )
-  | App (_func, _arg) -> failwith "infer type of function application"
-  | Lam (_domainType, _body) -> failwith "infer type of a function"
+  | App (func, arg) -> (
+    let func_type = inferType env localCtx func in
+    let inferred_arg_type = inferType env localCtx arg in
+    match func_type with
+      | Forall (expected_arg_type, _) -> 
+          (* TODO: replace this with checking for definitional equality *)
+        if expected_arg_type = inferred_arg_type then
+          (* TODO: check if this is the right approach *)
+          (* The actual type of the function application can depend on the
+          actual value that it's evaluated at so we need to evaluate the function
+          at the given argument and then infer the type of the result *)
+          let body_evaluated_at_arg = match func with
+            | Lam (_, body) -> subst_bvar body 0 arg
+            | _ -> failwith "Expected Lam for `func` in application" in
+          inferType env localCtx body_evaluated_at_arg
+        else failwith "Function called with invalid argument type"
+      | _ -> failwith "Tried to apply non-function to an argument"
+  )
+  | Lam (domainType, body) -> (
+      (* Put the type of the argument at the start of the local context list so
+      that Bvar 0 in the lambda points to domainType, and Bvar 1 (for example)
+      would point to what is now Bvar 0 (since Bvar 0 at that point would point
+      to this function's argument and Bvar 1 would point to the next bound variable)*)
+      let new_local_ctx = domainType :: localCtx in
+      let ret_type = inferType env new_local_ctx body in
+      Forall (domainType, ret_type)
+    )
   | Forall (_domainType, _returnType) -> failwith "infer type of a forall"
   | Sort _level -> failwith "infer type of a sort"
--- test/test_system_e_kernel.ml
+++ test/test_system_e_kernel.ml
@@ -2,6 +2,7 @@
 open System_e_kernel
 open Term
 open Infer
+open Printexc
 
 let str_contains s sub =
   let n = String.length sub in
@@ -17,6 +18,7 @@ let mk_env () =
   Hashtbl.add env "Point" (Sort 0);
   Hashtbl.add env "Line" (Sort 0);
   Hashtbl.add env "p" (Const "Point");
+  Hashtbl.add env "l" (Const "Line");
   env
 
 let test_const_lookup () =
@@ -67,11 +69,81 @@ let test_const_unknown_fails () =
     assert false
   with Failure msg -> assert (str_contains msg "unknown constant")
 
+let test_infer_function_type () =
+  let env = mk_env () in 
+
+  (* environment used in test contains a variable "p" with type Point *)
+  let const_func = Lam (Const "Point", Const "p") in
+  assert ((inferType env [] const_func) = Forall (Const "Point", Const "Point"));
+
+  let identity_func = Lam (Const "Point", Bvar 0) in
+  assert ((inferType env [] identity_func) = Forall (Const "Point", Const "Point"));
+  
+  let func_returning_outer_bvar = Lam (Const "Line", Bvar 1) in
+  assert ((inferType env [Const "Point"] func_returning_outer_bvar) = Forall (Const "Line", Const "Point"));
+
+  let generic_func = Lam (Sort 1, Lam (Bvar 0, Bvar 0)) in
+  assert ((inferType env [] generic_func) = Forall (Sort 1, Forall (Bvar 0, Bvar 0)))
+
+let test_infer_function_application () =
+  let env = mk_env () in
+  
+  (* TODO: try testing case where return type is computed from argument? *)
+
+  let const_func_app = App (Lam (Const "Point", Const "p"), Bvar 0) in
+  assert ((inferType env [Const "Point"] const_func_app) = Const "Point");
+  try
+    ignore (inferType env [Const "Line"] const_func_app);
+    assert false
+  with Failure msg -> assert (str_contains msg "invalid argument type");
+
+  let identity_func_app = App (Lam (Const "Point", Bvar 0), Const "p") in
+  assert ((inferType env [] identity_func_app) = Const "Point");
+
+  let application_with_non_function = App (Const "p", Const "l") in
+  try
+    ignore (inferType env [] application_with_non_function);
+    assert false
+  with Failure msg -> assert (str_contains msg "apply non-function to an argument");
+
+  (* TODO: should Point be a Sort 0 or a Sort 1? *)
+  (* Corresponds to the expression `(fun (A: Type) -> fun (x: A) -> x) Point` which
+  is equivalent to `fun (x: Point) -> x`, so we'd expect a type of `Point => Point` *)
+  let generic_func_app = App (Lam (Sort 0, Lam (Bvar 0, Bvar 0)), Const "Point") in
+  let inferred_type = inferType env [] generic_func_app in
+  assert (inferred_type = Forall (Const "Point", Const "Point"))
+
+let test_subst_bvar () = 
+  assert (subst_bvar (Const "Point") 0 (Const "p") = Const "Point");
+  assert (subst_bvar (Bvar 0) 0 (Const "p") = Const "p");
+  assert (subst_bvar (Bvar 0) 1 (Const "l") = Bvar 0);
+  assert (subst_bvar (Bvar 0) 0 (Bvar 5) = Bvar 5);
+  (* The inner Bvar 0 refers to the argument to the Lam, so Bvar 0 for the topmost
+  expression is Bvar 1 from the point of view of the inner expression *)
+  assert (subst_bvar (Lam (Bvar 0, Bvar 0)) 0 (Const "Point") = Lam (Const "Point", Bvar 0));
+  assert (subst_bvar (Lam (Bvar 0, Bvar 1)) 0 (Const "Point") = Lam (Const "Point", Const "Point"));
+  assert (subst_bvar (Lam (Bvar 0, Bvar 1)) 1 (Const "Point") = Lam (Bvar 0, Bvar 1));
+
+  (* Forall isn't really a function (in the sense that we can't apply it to an expression) 
+  but the domain type does act as a bound variable similarly to a Lam, meaning that we
+  need to recursively substitute in a Forall as well using the same rules*)
+  assert (subst_bvar (Forall (Bvar 0, Bvar 0)) 0 (Sort 5) = Forall (Sort 5, Bvar 0));
+  assert (subst_bvar (Forall (Bvar 0, Bvar 1)) 0 (Sort 5) = Forall (Sort 5, Sort 5));
+  assert (subst_bvar (Forall (Bvar 0, Bvar 1)) 1 (Sort 5) = Forall (Bvar 0, Bvar 1))
+
+
 let () =
+  (* Taken from https://stackoverflow.com/questions/65868770/lack-of-information-when-ocaml-crashes#comment128358969_65873074,
+  turns on stack traces *)
+  record_backtrace true;
+
   test_const_lookup ();
   test_fvar_lookup ();
   test_fvar_unknown_fails ();
   test_bvar_stack ();
   test_bvar_out_of_scope_fails ();
   test_const_unknown_fails ();
+  test_infer_function_type ();
+  test_subst_bvar ();
+  test_infer_function_application ();
   print_endline "All inferType tests passed."
