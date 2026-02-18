75b3f02d575fb519dadc34381b90761ac2cbb25f
Talia Ringer <talia@dependenttyp.es>
2026-02-18 11:58:49 -0600

    add custom exception type file

---
+++ lib/exceptions.ml
@@ -0,0 +1,10 @@
+open Term
+(* TODO make .mli file, move this to a different folder from kernel *)
+
+(* --- Exception types --- *)
+
+exception TypeError of term (* TODO take other needed to print arguments *)
+
+(* --- Showing error messages --- *)
+
+(* TODO *)
