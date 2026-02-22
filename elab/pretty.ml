5ffdc5220b665f85c7f22bba7ea933fa1b2cdd4d
Priyam Sahoo <priyamsahoo.26@gmail.com>
2026-02-22 12:03:56 -0600

    prettify elaborator terms

---
+++ elab/pretty.ml
@@ -0,0 +1,52 @@
+(** Pretty-printing for elaborator terms.  Only depends on [Term] and [Decl]. *)
+
+open Term
+open Decl
+
+let sort_to_string = function
+  | 0 -> "Prop"
+  | 1 -> "Type"
+  | n -> "Type" ^ string_of_int n
+
+let is_atomic = function
+  | Name _ | Hole | Sort _ -> true
+  | Fun _ | Arrow _ | App _ -> false
+
+(** Flatten application spine. *)
+let rec flatten_app t =
+  match t with
+  | App (f, a) ->
+      let head, args = flatten_app f in
+      (head, args @ [a])
+  | other -> (other, [])
+
+let rec term_to_string (t : term) : string =
+  match t with
+  | Name x -> x
+  | Hole -> "_"
+  | Sort n -> sort_to_string n
+  | Fun (x, ty, body) ->
+      let ty_s = term_to_string ty in
+      let body_s = term_to_string body in
+      "fun (" ^ x ^ " : " ^ ty_s ^ ") => " ^ body_s
+  | Arrow (x, ty, ret) ->
+      let ty_s = term_to_string ty in
+      let ret_s = term_to_string ret in
+      if x = "" then ty_s ^ " -> " ^ ret_s
+      else "(" ^ x ^ " : " ^ ty_s ^ ") -> " ^ ret_s
+  | App _ ->
+      let head, args = flatten_app t in
+      let head_s = term_to_string head in
+      let args_s =
+        List.map
+          (fun a ->
+            let s = term_to_string a in
+            if is_atomic a then s else "(" ^ s ^ ")")
+          args
+      in
+      (match args_s with [] -> head_s | _ -> head_s ^ " " ^ String.concat " " args_s)
+
+let decl_to_string = function
+  | Axiom (name, ty) -> "Axiom " ^ name ^ " : " ^ term_to_string ty
+  | Theorem (name, ty, proof) ->
+      "Theorem " ^ name ^ " : " ^ term_to_string ty ^ " := " ^ term_to_string proof
