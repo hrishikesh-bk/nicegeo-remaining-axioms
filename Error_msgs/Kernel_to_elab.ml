a353c18b4f1f6a4da5ee532093425801bfc427e0
Kevin Fisher <kfish610@gmail.com>
2026-03-02 11:58:03 -0600

    Format existing code with ocamlformat

--- Error_msgs/Kernel_to_elab.ml
+++ Error_msgs/Kernel_to_elab.ml
@@ -3,8 +3,6 @@ module KExceptions = System_e_kernel.Exceptions
 module KTerm = System_e_kernel.Term
 module E = Error
 
-
-
 let locals_to_pairs (ctx : KTerm.localcontext) : (string * string) list =
   (* ctx is a Hashtbl: string -> term *)
   Hashtbl.fold (fun k v acc -> (k, term_to_string v) :: acc) ctx []
@@ -14,7 +12,8 @@ let raise_as_elab_error ?decl_name (exn : exn) : 'a =
   | KExceptions.TypeError info ->
       let ec =
         E.empty_context
-        |> (fun c -> match decl_name with None -> c | Some d -> E.with_decl_name d c)
+        |> (fun c ->
+        match decl_name with None -> c | Some d -> E.with_decl_name d c)
         |> E.with_term_name (term_to_string info.trm)
         |> E.with_local_context (locals_to_pairs info.ctx)
         |> E.with_note (type_err_to_string info)
@@ -24,7 +23,8 @@ let raise_as_elab_error ?decl_name (exn : exn) : 'a =
   | KExceptions.RedError info ->
       let ec =
         E.empty_context
-        |> (fun c -> match decl_name with None -> c | Some d -> E.with_decl_name d c)
+        |> (fun c ->
+        match decl_name with None -> c | Some d -> E.with_decl_name d c)
         |> E.with_term_name (term_to_string info.trm)
         |> E.with_local_context (locals_to_pairs info.ctx)
         |> E.with_note (red_err_to_string info)
@@ -34,11 +34,9 @@ let raise_as_elab_error ?decl_name (exn : exn) : 'a =
   | _ ->
       E.raise_internal "Non-kernel exception escaped into kernel_to_elab_error"
 
-
 (*how to use: Anywhere in the elab where we call kernel infer/reduction:
       try
        Kernel.infer env ctx t
       with e ->
        Kernel_to_elab_error.raise_as_elab_error ~decl_name:"myDecl" e)
        *)
-       
--- bin/main.ml
+++ bin/main.ml
@@ -8,23 +8,25 @@ let () =
     Printf.eprintf "Usage: %s <filename>\n" Sys.argv.(0);
     exit 1
   end;
-  
+
   let filename = Sys.argv.(1) in
   let ic = open_in filename in
   let lexbuf = Lexing.from_channel ic in
-  
+
   let decls : E_elab.Decl.declaration list =
-    try
-      E_elab.Parser.main E_elab.Lexer.token lexbuf
-    with
+    try E_elab.Parser.main E_elab.Lexer.token lexbuf with
     | Failure msg ->
         Printf.eprintf "Parsing error: %s\n" msg;
-        Printf.eprintf "At offset: %d-%d\n" (Lexing.lexeme_start lexbuf) (Lexing.lexeme_end lexbuf);
+        Printf.eprintf "At offset: %d-%d\n"
+          (Lexing.lexeme_start lexbuf)
+          (Lexing.lexeme_end lexbuf);
         close_in ic;
         exit 1
     | exn ->
         Printf.eprintf "Parsing exception: %s\n" (Printexc.to_string exn);
-        Printf.eprintf "At offset: %d-%d\n" (Lexing.lexeme_start lexbuf) (Lexing.lexeme_end lexbuf);
+        Printf.eprintf "At offset: %d-%d\n"
+          (Lexing.lexeme_start lexbuf)
+          (Lexing.lexeme_end lexbuf);
         close_in ic;
         exit 1
   in
@@ -32,7 +34,18 @@ let () =
   let env = Elab.create_with_env () in
 
   (* Process proof.txt *)
-  let all_decls_good = List.fold_left (fun x decl -> try Elab.process_decl env decl; x with Failure msg -> print_endline ("Error adding declaration " ^   Decl.decl_name decl ^ ": " ^ msg); false) true decls in
+  let all_decls_good =
+    List.fold_left
+      (fun x decl ->
+        try
+          Elab.process_decl env decl;
+          x
+        with Failure msg ->
+          print_endline
+            ("Error adding declaration " ^ Decl.decl_name decl ^ ": " ^ msg);
+          false)
+      true decls
+  in
   if not all_decls_good then begin
     print_endline "Error(s) encountered while processing proof file. Exiting.";
     exit 1
