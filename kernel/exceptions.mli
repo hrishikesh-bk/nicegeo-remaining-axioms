ec3ac8db8cb0da27bd75e073eceebb1a48718222
Talia Ringer <talia@dependenttyp.es>
2026-03-04 12:00:26 -0600

    odoc compatibility

--- kernel/exceptions.mli
+++ kernel/exceptions.mli
@@ -1,21 +1,21 @@
-(*
- * This file contains the kernel-side exception types, to be passed
- * to the elaborator for processing.
+(**
+   This file contains the kernel-side exception types, to be passed
+   to the elaborator for processing.
  *)
 open Term
 
 (* --- Exception types --- *)
 
-(*
- * Kinds of type errors that the kernel can throw.
- *
- * UnknownConstError: Unknown constant where the string encodes the name
- * UnknownFreeVarError: Unknown free variable where the string encodes the name
- * BoundVarScopeError: Bound variable scope error, where the int is the index
- * AppArgTypeError: Type error for the argument of a function application, where
- * the terms represent (1) the function, (2) the argument, (3) the type of the 
- * function, (4) the expected type of the argument, and (5) the actual type of 
- * the argument.
+(**
+   Kinds of type errors that the kernel can throw.
+ 
+   UnknownConstError: Unknown constant where the string encodes the name
+   UnknownFreeVarError: Unknown free variable where the string encodes the name
+   BoundVarScopeError: Bound variable scope error, where the int is the index
+   AppArgTypeError: Type error for the argument of a function application, where
+   the terms represent (1) the function, (2) the argument, (3) the type of the 
+   function, (4) the expected type of the argument, and (5) the actual type of 
+   the argument.
  *)
 type type_error_kind =
   | UnknownConstError of string
@@ -26,9 +26,7 @@ type type_error_kind =
   | LamDomainError
   | ForallSortError of term * term
 
-(*
- * Type error information that the kernel passes on.
- *)
+(** Type error information that the kernel passes on. *)
 type type_error_info =
   {
     env : environment;
@@ -37,6 +35,6 @@ type type_error_info =
     err_kind : type_error_kind
   }
 
-(* Exceptions that the kernel may raise, using the above information. *)
+(** Exceptions that the kernel may raise, using the above information. *)
 exception TypeError of type_error_info
 
